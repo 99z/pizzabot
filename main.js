02c75e65a31066a04da272a64fce6e0491a65904
Nicolas S. Montanaro <nsmon@rbox.co>
2018-12-02 20:54:26 -0500

    minor input handling change

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const pizzabot = require('./pizzabot');
 
+if (!process.argv[2]) {
+    console.error(`usage: node main.js "NxN (x1, y1) (x2, y2) ...`);
+    process.exit(1);
+}
+
 let stripped = pizzabot.stripInputCoords(process.argv[2]);
 pizzabot.validateInput(stripped);
 let normalized = pizzabot.normalizeCoords(stripped);
